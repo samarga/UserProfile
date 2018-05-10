444eb6874552e3e8fbaa457e5d442e68e0464699
Sheila Amarga <sheila.amarga@nike.com>
2018-05-10 13:11:36 -0700

    Added textviews and image view using findeViewById method and setText/setImage methods in MainActivity.java

--- app/src/main/java/com/example/samarg/userprofile/MainActivity.java
+++ app/src/main/java/com/example/samarg/userprofile/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.samarg.userprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView pictureImageView = (ImageView) findViewById(R.id.profile_picture);
+        pictureImageView.setImageResource(R.drawable.picture);
+
+        TextView nameTextView = (TextView) findViewById(
+                R.id.name);
+        nameTextView.setText("Jessica");
+
+        TextView birthdayTextView = (TextView) findViewById(
+                R.id.birthday);
+        birthdayTextView.setText("01/01/1970");
+
+        TextView countryTextView = (TextView) findViewById(
+                R.id.country);
+        countryTextView.setText("United States of America");
     }
 }
